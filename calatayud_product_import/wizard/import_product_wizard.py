c42ff5ab9a67dcfd8257b972305269f13820f276
Manuel Calero <manuelcalerosolis@gmail.com>
2023-07-30 13:38:36 +0200

    create product attribute

--- calatayud_product_import/wizard/import_product_wizard.py
+++ calatayud_product_import/wizard/import_product_wizard.py
@@ -43,11 +43,29 @@ class CalatayudProductImport(models.TransientModel):
             decoded_data = base64.decodebytes(import_file)
             book = xlrd.open_workbook(file_contents=decoded_data)
             sheet = book.sheet_by_index(0)
+            product_attribute_color = self._search_or_create_product_attribute('Color')
             for row in range(1, sheet.nrows):
-                if sheet.cell_value(row, 1):
-                    self._search_or_create_attribute(sheet, row)
-                else:
-                    self._search_or_create_product_template(sheet, row)
+                name = sheet.cell_value(row, 0)
+                product_attribute_value = sheet.cell_value(row, 1)
+                description_sale = sheet.cell_value(row, 2)
+                product_tag = sheet.cell_value(row, 3)
+                category = sheet.cell_value(row, 4)
+                if not name:
+                    return
+
+                product_template = self._search_or_create_product_template(
+                    name, description_sale, category, product_tag
+                )
+                if not product_template:
+                    return
+
+                product_attribute_color_value = self._search_or_create_product_attribute_value(
+                    product_attribute_color, product_attribute_value
+                )
+                if not product_attribute_color_value:
+                    self._search_or_create_product_attribute_line(
+                        product_template, product_attribute_color, product_attribute_color_value
+                    )
 
         except xlrd.XLRDError:
             raise ValidationError(
@@ -56,14 +74,12 @@ class CalatayudProductImport(models.TransientModel):
         except Exception as e:
             raise e
 
-    def _search_or_create_product_template(self, sheet, row):
-        name = sheet.cell_value(row, 0)
+    def _search_or_create_product_template(self, name, description_sale, category, product_tag):
         if not name:
             return
         result = self.env["product.template"].search([("name", "=", name)])
         if result:
             return result
-        description_sale = sheet.cell_value(row, 2)
         product_template = {
             'detailed_type': 'product',
             'invoice_policy': 'delivery',
@@ -71,18 +87,17 @@ class CalatayudProductImport(models.TransientModel):
             'description_sale': description_sale,
         }
 
-        category_id = self._search_or_create_category(sheet, row)
+        category_id = self._search_or_create_category(category)
         if category_id:
             product_template['categ_id'] = category_id.id
 
-        product_tag_ids = self._search_or_create_product_tag(sheet, row)
+        product_tag_ids = self._search_or_create_product_tag(product_tag)
         if product_tag_ids:
             product_template['product_tag_ids'] = [(6, 0, product_tag_ids.ids)]
 
-        self.env["product.template"].create(product_template)
+        return self.env["product.template"].create(product_template)
 
-    def _search_or_create_category(self, sheet, row):
-        category = sheet.cell_value(row, 4)
+    def _search_or_create_category(self, category):
         if not category:
             return
         result = self.env["product.category"].search([("name", "=", category)])
@@ -90,14 +105,7 @@ class CalatayudProductImport(models.TransientModel):
             return result
         return self.env["product.category"].create({"name": category})
 
-    def _search_or_create_attribute(self, sheet, row):
-        product_attribute = self._search_or_create_product_attribute('Color')
-        if not product_attribute:
-            return
-        product_attribute_value = sheet.cell_value(row, 1)
-        self._search_or_create_product_attribute_value(product_attribute, product_attribute_value)
-    def _search_or_create_product_tag(self, sheet, row):
-        product_tag = sheet.cell_value(row, 3)
+    def _search_or_create_product_tag(self, product_tag):
         if not product_tag:
             return
         result = self.env["product.tag"].search([("name", "=", product_tag)])
@@ -114,11 +122,11 @@ class CalatayudProductImport(models.TransientModel):
         )
         return result
 
-    def _search_or_create_product_attribute_value(self, product_attribute, product_attribute_value):
-        product_attribute_id = product_attribute[0].id
+    def _search_or_create_product_attribute_value(self, product_attribute_color, product_attribute_value):
+        product_attribute_color_id = product_attribute_color[0].id
         result = self.env["product.attribute.value"].search(
             [
-                ("attribute_id", "=", product_attribute_id),
+                ("attribute_id", "=", product_attribute_color_id),
                 ("name", "=", product_attribute_value),
             ]
         )
@@ -126,7 +134,25 @@ class CalatayudProductImport(models.TransientModel):
             return result
         return self.env["product.attribute.value"].create(
             {
-                "attribute_id": product_attribute_id,
+                "attribute_id": product_attribute_color_id,
                 "name": product_attribute_value,
+            }
+        )
+
+    def _search_or_create_product_attribute_line(self, product_template, product_attribute_color, product_attribute_color_value):
+        result = self.env["product.template.attribute.line"].search(
+            [
+                ("product_tmpl_id", "=", product_template.id),
+                ("attribute_id", "=", product_attribute_color.id),
+                ("value_ids", "in", product_attribute_color_value.id),
+            ]
+        )
+        if result:
+            return result
+        return self.env["product.template.attribute.line"].create(
+            {
+                "product_tmpl_id": product_template.id,
+                "attribute_id": product_attribute_color.id,
+                "value_ids": [(6, 0, [product_attribute_color_value.id])],
             }
         )
