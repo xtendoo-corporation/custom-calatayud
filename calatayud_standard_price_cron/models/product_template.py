2485f8f97117a5bcd0eddfab79631f2317951c39
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-04-10 12:39:31 +0200

    with variants

--- calatayud_standard_price_cron/models/product_template.py
+++ calatayud_standard_price_cron/models/product_template.py
@@ -9,13 +9,19 @@ class ProductTemplate(models.Model):
 
     @api.model
     def cron_action_update_standard_price(self):
-        print("/"*50)
+        print("Cron action run********************************************")
         almacenes_company_id = self.env['res.company'].search([('vat', '=', 'ESB41271206')], limit=1)
         tienda_company_id = self.env['res.company'].search([('vat', '=', 'ESB91823187')], limit=1)
-        products = self.env['product.template'].search([])
-        for product in products:
+
+        products_template = self.env['product.template'].search([("company_id", "=", False)])
+        for product in products_template:
             almacenes_product = product.with_company(almacenes_company_id)
             tienda_product = product.with_company(tienda_company_id)
             if almacenes_product.standard_price != tienda_product.standard_price:
                 tienda_product.standard_price = almacenes_product.standard_price
+            for variant in product.product_variant_ids:
+                almacenes_variant = variant.with_company(almacenes_company_id)
+                tienda_variant = variant.with_company(tienda_company_id)
+                if almacenes_variant.standard_price != tienda_variant.standard_price:
+                    tienda_variant.standard_price = almacenes_variant.standard_price
 
