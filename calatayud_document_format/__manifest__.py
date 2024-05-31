4af634e35d2c0111e6bc1b45d3dcd06c4f4b6869
GonzalezJimenezSalvador <salvador.gon.jim@gmail.com>
2024-05-31 14:38:36 +0200

    new views for documents

---
+++ calatayud_document_format/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Document format Calatayud",
+    "version": "16.0",
+    "author": "Salvador Gonzalez (https://xtendoo.es)",
+    "category": "Calatayud",
+    "license": "AGPL-3",
+    'depends': ['base_setup', 'product', 'analytic', 'portal', 'digest', 'sale', 'base', 'stock', 'stock_picking_report_valued'],
+    "data": [
+        "views/invoice_document.xml",
+        "views/sale_order_document.xml",
+        "views/report_deliveryslip.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
