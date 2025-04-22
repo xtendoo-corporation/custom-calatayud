dd82d302e79dacdfbcbf439aef1fbbaa4d1072c4
guillermo2005200 <91117754+guillermo2005200@users.noreply.github.com>
2025-04-22 12:30:39 +0200

    arreglado

--- purchase_price_fix/models/purchase_order_line.py
+++ purchase_price_fix/models/purchase_order_line.py
@@ -7,6 +7,6 @@ class PurchaseOrderLine(models.Model):
     def _compute_price_unit_and_date_planned_and_name(self):
         super()._compute_price_unit_and_date_planned_and_name()
 
-        for line in self.filter(lambda l: l.product_id and not l.display_type, self):
+        for line in self.filtered(lambda l: l.product_id and not l.display_type):
             if not line.price_unit or line.price_unit == 0:
                 line.price_unit = line.product_id.standard_price
