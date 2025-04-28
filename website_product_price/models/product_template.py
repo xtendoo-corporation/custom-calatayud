06a51293ad433b3bcc9ac4f645765b5f411a4c9c
Guillermo Barcena <barcenalopezguillermo@gmail.com>
2025-04-28 07:52:20 +0200

    Permite compra sin login en tienda

--- website_product_price/models/product_template.py
+++ website_product_price/models/product_template.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
 from odoo import models, api
 
+
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
-
     @api.model
     def _price_with_tax_computed(
         self, price, product_taxes, taxes, company_id, pricelist, product, partner
     ):
         website = self.env['website'].get_current_website()
-
-
         if website.id == 1:
             return price
-
         res = super()._price_with_tax_computed(
             price, product_taxes, taxes, company_id, pricelist, product, partner
         )
-
         return res
