97b83b07bd0e979a685c0f1327156aec410168a9
Guillermo Barcena <barcenalopezguillermo@gmail.com>
2025-09-19 07:46:06 +0200

    botones redes sociales

--- calatayud_website_product_stock_and_tax/controllers/main.py
+++ calatayud_website_product_stock_and_tax/controllers/main.py
@@ -7,6 +7,7 @@ class WebsiteSaleStockCustom(WebsiteSale):
     @http.route(['/shop/product/<model("product.template"):product>'], type='http', auth="public", website=True)
     def product(self, product, category='', search='', **kwargs):
         response = super().product(product, category=category, search=search, **kwargs)
+        print("*"*100)
 
         website = request.website
         if website.id == 3 and product.qty_available <= 0:
