3c543dc922e0132e2988d323c92acfee4193670c
Guillermo Barcena <barcenalopezguillermo@gmail.com>
2025-09-25 10:56:22 +0200

    group created, but dont work the rule

---
+++ calatayud_security_rules_custom/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Calatayud security_rules_custom',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Control de acceso para asignación de tarifas de venta',
+    'description': """
+        Módulo que permite controlar quién puede asignar tarifas de venta a contactos.
+        Solo usuarios con el grupo 'administrador_tarifas' pueden realizar esta acción.
+    """,
+    'author': 'Guillermo Bárcena López',
+    'depends': ['base', 'sale', 'product'],
+    'data': [
+        'security/security.xml',
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
