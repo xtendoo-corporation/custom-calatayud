90b609e675f0425db9a066cd838c49840a72b5f6
Guillermo Bárcena López <barcenalopezguillermo@gmail.com>
2025-10-22 13:25:06 +0200

    partner price ignored

--- purchase_price_fix/models/purchase_order_line.py
+++ purchase_price_fix/models/purchase_order_line.py
@@ -8,5 +8,4 @@ class PurchaseOrderLine(models.Model):
         super()._compute_price_unit_and_date_planned_and_name()
 
         for line in self.filtered(lambda l: l.product_id and not l.display_type):
-            if not line.price_unit or line.price_unit == 0:
-                line.price_unit = line.product_id.standard_price
+            line.price_unit = line.product_id.standard_price
